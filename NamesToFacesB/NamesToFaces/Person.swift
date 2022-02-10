f1c0f74171b6ed661c813980c6fc397e9835661d
AlexanderFilippov <philalex196401@gmail.com>
2022-02-10 13:02:08 +0100

    Set up 2 copies of NamesToFaces for modification

---
+++ NamesToFacesB/NamesToFaces/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Alexander Filippov on 3.2.22..
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    
+}
