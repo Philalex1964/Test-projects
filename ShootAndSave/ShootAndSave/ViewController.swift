fdbec6de29baebb5418dcdb0107503a72e0512be
AlexanderFilippov <philalex196401@gmail.com>
2022-02-13 17:20:54 +0100

    Set up a Shoot-n-Save

---
+++ ShootAndSave/ShootAndSave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShootAndSave
+//
+//  Created by Alexander Filippov on 13.2.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
