5f5e4490eecaaba7151c241cac1c27a949a9a3a4
AlexanderFilippov <philalex196401@gmail.com>
2022-02-15 11:58:38 +0100

    set up a new Project - Instafilter

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Alexander Filippov on 15.2.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
