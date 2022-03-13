810d82f409eb350d23c565b4a80c659e25bf1b87
AlexanderFilippov <philalex196401@gmail.com>
2022-03-13 20:03:04 +0100

    Added pins with MKPinAnnototionView and UIAlert with desciption of the place

---
+++ CapitalCities/CapitalCities/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Alexander Filippov on 13.3.22..
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
