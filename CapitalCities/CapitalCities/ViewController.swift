e5d852cbd811646c8b201c3732e29b4d29ba5263
AlexanderFilippov <philalex196401@gmail.com>
2022-03-13 21:20:29 +0100

    Added choice for map type. Changed pin color. Prepared url for wikipedia  in webView

--- CapitalCities/CapitalCities/ViewController.swift
+++ CapitalCities/CapitalCities/ViewController.swift
@@ -14,17 +14,14 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
-        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
-        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
-        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
-        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
-        let petrovaradin = Capital(title: "Petrovaradin", coordinate: CLLocationCoordinate2D(latitude: 45.2436563, longitude: 19.8837914), info: "I just like it.")
-//        mapView.addAnnotation(london)
-//        mapView.addAnnotation(oslo)
-//        mapView.addAnnotation(paris)
-//        mapView.addAnnotation(rome)
-//        mapView.addAnnotation(washington)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(chooseMapType))
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/London"))
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/Oslo"))
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/Paris"))
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/Rome"))
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/Washington"))
+        let petrovaradin = Capital(title: "Petrovaradin", coordinate: CLLocationCoordinate2D(latitude: 45.2436563, longitude: 19.8837914), info: "I just like it.", url: URL(fileURLWithPath: "https://en.wikipedia.org/wiki/Petrovaradin"))
         
         mapView.addAnnotations([london, oslo, paris, rome, washington, petrovaradin])
     }
@@ -34,7 +31,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
         let identifier = "Capital"
 
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
 
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
@@ -46,6 +43,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
             annotationView?.annotation = annotation
         }
 
+        annotationView?.pinTintColor = .purple
+        
         return annotationView
     }
 
@@ -56,7 +55,28 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let placeInfo = capital.info
         
         let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Open Wikipedia?", style: .default) { [weak self] _ in
+            let vc = self?.storyboard?.instantiateViewController(withIdentifier: "Wiki")
+            
+            
+        })
         ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func chooseMapType() {
+        let ac = UIAlertController(title: "Choose map type", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Usual Map", style: .default) { [weak self] _ in
+            self?.mapView.mapType = .standard
+        })
+        ac.addAction(UIAlertAction(title: "Satellite Map", style: .default) { [weak self] _ in
+            self?.mapView.mapType = .satellite
+        })
+        ac.addAction(UIAlertAction(title: "Hybrid Map", style: .default) { [weak self] _ in
+            self?.mapView.mapType = .hybrid
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
     }
 }
