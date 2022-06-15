7d2adc4d477453af32bc4ba9742b8abdd7339afd
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-15 12:32:11 +0200

    Creating our first unit test using XCTest Loading our data and splitting up words: filter() Counting unique strings in an array measure(): How to optimize our slow code and adjust the baseline

--- UnitTestsSwift/UnitTestsSwift/ViewController.swift
+++ UnitTestsSwift/UnitTestsSwift/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+var playData = PlayData()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +23,24 @@ class ViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return playData.allWords.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
-
+        let word = playData.allWords[indexPath.row]
+        cell.textLabel!.text = word
+        cell.detailTextLabel!.text = "\(playData.wordCounts.count(for: word))"
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
--- UnitTestsSwift/UnitTestsSwiftTests/UnitTestsSwiftTests.swift
+++ UnitTestsSwift/UnitTestsSwiftTests/UnitTestsSwiftTests.swift
@@ -9,15 +9,36 @@ import XCTest
 @testable import UnitTestsSwift
 
 class UnitTestsSwiftTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 18440, "allWords was not 18440")
+    }
+    
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        //        XCTAssertEqual(playData.wordCounts["home"], 174, "Home does not appear 174 times")
+        //        XCTAssertEqual(playData.wordCounts["fun"], 4, "Fun does not appear 4 times")
+        //        XCTAssertEqual(playData.wordCounts["mortal"], 41, "Mortal does not appear 41 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "home"), 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "mortal"), 41, "Mortal does not appear 41 times")
+    }
+    
+    func testWordsLoadQuickly() {
+        measure {
+            _ = PlayData()
+        }
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +46,12 @@ class UnitTestsSwiftTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            _ = PlayData()
         }
     }
-
+    
 }
